59a1f92b0b439627fd085efb8524219129eed25f
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2025-07-16 17:49:20 +0100

    Add clear screen functionality

--- src/editor.rs
+++ src/editor.rs
@@ -1,25 +1,69 @@
-use crossterm::event::{Event::Key, KeyCode::Char, read};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::event::{Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers, read};
+use terminal::Terminal;
 
-pub struct Editor {}
+mod terminal;
+
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
-    pub fn run() {
-        enable_raw_mode().unwrap();
+    pub const fn default() -> Self {
+        Self { should_quit: false }
+    }
+    pub fn run(&mut self) {
+        Terminal::initialize().unwrap();
+        let result = self.repl();
+        Terminal::terminate().unwrap();
+        result.unwrap();
+    }
+
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{event:?} \r");
-                    if let Char(c) = event.code {
-                        if c == 'q' {
-                            break;
-                        }
-                    }
-                }
-                Err(e) => println!("Error: {e}"),
-                _ => (),
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
+            }
+            let event = read()?;
+            self.evaluate_event(&event);
+        }
+        Ok(())
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            Terminal::clear_screen()?;
+            print!("Goodbye.\r\n");
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(0, 0)?;
+        }
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code: Char('q'),
+            modifiers: KeyModifiers::CONTROL,
+            ..
+        }) = event
+        {
+            self.should_quit = true;
+        }
+    }
+
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let (_, rows) = crossterm::terminal::size()?;
+
+        for i in 0..rows {
+            print!("~\r");
+            if i + 1 < rows {
+                print!("\r\n");
             }
         }
-        disable_raw_mode().unwrap();
+
+        crossterm::cursor::MoveTo(0, 0);
+
+        Ok(())
     }
 }
