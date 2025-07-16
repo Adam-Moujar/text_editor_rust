ce4c9e114142bd04ac465adf4b4993d4917b1207
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2025-07-16 12:02:32 +0100

    Refactor code

---
+++ src/editor.rs
@@ -0,0 +1,25 @@
+use crossterm::event::{Event::Key, KeyCode::Char, read};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run() {
+        enable_raw_mode().unwrap();
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{event:?} \r");
+                    if let Char(c) = event.code {
+                        if c == 'q' {
+                            break;
+                        }
+                    }
+                }
+                Err(e) => println!("Error: {e}"),
+                _ => (),
+            }
+        }
+        disable_raw_mode().unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
+#![warn(clippy::all, clippy::pedantic)]
+
+mod editor;
+
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-
-        println!("{}", c);
-
-        if c == 'q' {
-            disable_raw_mode().unwrap();
-            break;
-        }
-    }
+    Editor::run();
 }
